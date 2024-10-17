4d3bee04dd17db069f1f7a75fe1ef217a6c2ce70
victormoncada2301 <victor_gregorio_2301@hotmail.com>
2024-10-16 21:38:37 -0300

    Set default routing and uppercase hero name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroListComponent } from './pages/hero-list/hero-list.component';
 
 const routes: Routes = [
-  { path: '', component: HeroListComponent }
+  { path: '', redirectTo: 'heroes-list', pathMatch: 'full' },
+  { path: 'heroes-list', component: HeroListComponent },  
 ];
 
 @NgModule({
--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -11,26 +11,26 @@ export class HeroService {
 
   constructor() {
     this.heroes = [
-      { id: this.id++, name: 'Spiderman', power: 'Wall-crawling' },
-      { id: this.id++, name: 'Superman', power: 'Super strength' },
-      { id: this.id++, name: 'Batman', power: 'Martial arts' },
-      { id: this.id++, name: 'Wonder Woman', power: 'Super agility' },
-      { id: this.id++, name: 'Flash', power: 'Super speed' },
-      { id: this.id++, name: 'Aquaman', power: 'Underwater breathing' },
-      { id: this.id++, name: 'Green Lantern', power: 'Energy constructs' },
-      { id: this.id++, name: 'Iron Man', power: 'High-tech suit' },
-      { id: this.id++, name: 'Thor', power: 'Thunder control' },
-      { id: this.id++, name: 'Hulk', power: 'Unlimited strength' },
-      { id: this.id++, name: 'Black Widow', power: 'Espionage' },
-      { id: this.id++, name: 'Doctor Strange', power: 'Magic' },
-      { id: this.id++, name: 'Scarlet Witch', power: 'Reality warping' },
-      { id: this.id++, name: 'Captain Marvel', power: 'Energy absorption' },
-      { id: this.id++, name: 'Ant-Man', power: 'Size manipulation' },
-      { id: this.id++, name: 'Wolverine', power: 'Regeneration' },
-      { id: this.id++, name: 'Deadpool', power: 'Immortality' },
-      { id: this.id++, name: 'Black Panther', power: 'Enhanced senses' },
-      { id: this.id++, name: 'Star-Lord', power: 'Master tactician' },
-      { id: this.id++, name: 'Groot', power: 'Regeneration' }
+      { id: this.id++, name: 'SPIDERMAN', power: 'Wall-crawling' },
+      { id: this.id++, name: 'SUPERMAN', power: 'Super strength' },
+      { id: this.id++, name: 'BATMAN', power: 'Martial arts' },
+      { id: this.id++, name: 'WONDER WOMAN', power: 'Super agility' },
+      { id: this.id++, name: 'FLASH', power: 'Super speed' },
+      { id: this.id++, name: 'AQUAMAN', power: 'Underwater breathing' },
+      { id: this.id++, name: 'GREEN LANTERN', power: 'Energy constructs' },
+      { id: this.id++, name: 'IRON MAN', power: 'High-tech suit' },
+      { id: this.id++, name: 'THOR', power: 'Thunder control' },
+      { id: this.id++, name: 'HULK', power: 'Unlimited strength' },
+      { id: this.id++, name: 'BLACK WIDOW', power: 'Espionage' },
+      { id: this.id++, name: 'DOCTOR STRANGE', power: 'Magic' },
+      { id: this.id++, name: 'SCARLET WITCH', power: 'Reality warping' },
+      { id: this.id++, name: 'CAPTAIN MARVEL', power: 'Energy absorption' },
+      { id: this.id++, name: 'ANT-MAN', power: 'Size manipulation' },
+      { id: this.id++, name: 'WOLVERINE', power: 'Regeneration' },
+      { id: this.id++, name: 'DEADPOOL', power: 'Immortality' },
+      { id: this.id++, name: 'BLACK PANTHER', power: 'Enhanced senses' },
+      { id: this.id++, name: 'STAR-LORD', power: 'Master tactician' },
+      { id: this.id++, name: 'GROOT', power: 'Regeneration' }
     ];
   }
 
